e0446507eef66793433561a6cf0437e666ff2c85
jwklong <jwklong@gmx.com>
2021-09-12 11:45:31 +0100

    fix now

--- js/mods/changelog-tab.js
+++ js/mods/changelog-tab.js
@@ -1,12 +1,14 @@
 Vue.component("changelog-tab", {
     template: `<div class="changelog-tab">
     <guide-item>
-        <template v-slot:title>α - v1.0.1</template>
-        <template v-slot:text>i just fixed some issues, tweaked some text and added a few more achivements.
-        </template>
-        <template v-slot:title>α - v1.0.0</template>
-        <template v-slot:text>uhm, prestige layer tweaks, a changelog, sabotage in meta layer, meta layer upgrades to 5 levels, 5 new themes and some other boring stuff. its how modding works lol.
-        </template>
+    <template v-slot:title>α - v1.0.1</template>
+    <template v-slot:text>fixes and other stuff also this version caused many bugs lol.
+    </template>
+    </guide-item>
+    <guide-item>
+    <template v-slot:title>α - v1.0.0</template>
+    <template v-slot:text>uhm, prestige layer tweaks, a changelog, sabotage in meta layer, meta layer upgrades to 5 levels, 5 new themes and some other boring stuff. its how modding works lol.
+    </template>
     </guide-item>
 </div>`
 })
